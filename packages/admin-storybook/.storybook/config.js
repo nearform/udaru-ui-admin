48bf7a1b3b00ed0e4f157470486754572bb29686
Jimmy Mintzer <jimmymintzer@users.noreply.github.com>
2018-02-16 12:47:50 -0500

    removed storybook as separate package, renamed both remaining packages

--- packages/admin-storybook/.storybook/config.js
+++
@@ -1,9 +0,0 @@
-import { configure, addDecorator } from '@storybook/react'
-import { withInfo } from '@storybook/addon-info'
-
-function loadStories() {
-  require('../stories/index.js')
-}
-
-addDecorator((story, context) => withInfo()(story)(context))
-configure(loadStories, module)
