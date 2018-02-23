b179d15bd4dd96cd0eda99556220381b98ad2e3f
Jimmy Mintzer <jimmymintzer@users.noreply.github.com>
2018-02-23 09:58:49 -0500

    update button styling for view teams

--- packages/udaru-admin-components/lib/toolbar.js
+++ packages/udaru-admin-components/lib/toolbar.js
@@ -54,7 +54,7 @@ class Toolbar extends React.Component {
                   }}
                 >
                   <Button
-                    bsStyle="info"
+                    bsStyle="primary"
                     onClick={this.props.onView}
                     disabled={this.props.disableView}
                     style={
